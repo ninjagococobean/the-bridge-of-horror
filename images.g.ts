5dbad2665c449d55a136d141f24958ddcca1a3c1
ninjagococobean <jasraj.saini@surreyschools.ca>
2025-05-24 09:58:35 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Young child Joe":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e e e e e . . . . . 
+. . . . . . e e d e e . . . . . 
+. . . . . . d f d f d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . f f f 1 f f f . . . . 
+. . . . f . f f f 1 f . f . . . 
+. . . . d . f f 1 f f . d . . . 
+. . . . . f f . . . f f . . . . 
+`;
+            case "image2":
+            case "Young child Benny":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 5 5 5 5 5 . . . . . 
+. . . . . . 5 5 d 5 5 . . . . . 
+. . . . . . d 8 d 8 d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . 2 2 2 5 2 2 2 . . . . 
+. . . . 2 . 2 5 4 5 2 . 2 . . . 
+. . . . d . 2 2 5 2 2 . d . . . 
+. . . . . 2 2 . . . 2 2 . . . . 
+`;
+            case "image3":
+            case "Young child Kai":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . . 2 2 d 2 2 . . . . . 
+. . . . . . d 7 d 7 d . . . . . 
+. . . . . . d d d d d . . . . . 
+. . . . . 9 9 1 1 9 9 9 . . . . 
+. . . . 9 . 1 1 1 1 9 . 9 . . . 
+. . . . d . 9 9 9 9 9 . d . . . 
+. . . . . 9 9 . . . 9 9 . . . . 
+`;
+            case "image4":
+            case "Young child Jay":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f d f f . . . . . . 
+. . . . . d a d a d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . 7 6 7 6 7 6 7 . . . . . 
+. . . 6 . 7 6 7 6 7 . 6 . . . . 
+. . . d . 6 7 6 7 6 . d . . . . 
+. . . . 6 7 . . . 7 6 . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
